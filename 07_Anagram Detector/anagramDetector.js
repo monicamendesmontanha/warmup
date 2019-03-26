adcb1f9b1d68a4fc7b54d157dddb8116e27f1b70
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-27 10:48:58 +1100

    solution of anagram detector

--- 07_Anagram Detector/anagramDetector.js
+++ 07_Anagram Detector/anagramDetector.js
@@ -10,3 +10,37 @@ In other words, given: "listen" and ["enlists" "google" "inlets" "banana"] the p
 Suggestions
 Start out by getting this working with discrete functions.
 // If you feel comfortable with that, try to put all your functions, collections, etc, into an object.*/
+
+let anagrams = [];
+
+const customSort = function(word) {
+  word = word.split("").sort().join("");
+
+  return word
+}
+
+const checkForAnagram = function(word, candidateWords) {
+  // word = word.split("");
+  // console.log(word);
+  // word = word.sort();
+  // console.log(word);
+  // word = word.join("");
+  // console.log(word);
+
+  let sortedWord = customSort(word)
+  console.log(sortedWord);
+
+  for ( let i = 0; i < candidateWords.length; i++ ) {
+    console.log("word before sort: ", candidateWords[i]);
+    let candidate = customSort(candidateWords[ i ]);
+    console.log("word after sort: ", candidate);
+
+    if (sortedWord === candidate) {
+      anagrams.push(candidateWords[i])
+    }
+  } //for
+
+  console.log(`The anagram of ${word} is ${anagrams}`);
+}; //chekcforanagram
+
+console.log(checkForAnagram("listen", ["enlists", "google", "inlets", "banana"]));
