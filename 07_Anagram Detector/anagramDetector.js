7ddcfdc4c510210fc98ff54103dbef7958f09352
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-27 10:49:57 +1100

    best solution of anagram detector

--- 07_Anagram Detector/anagramDetector.js
+++ 07_Anagram Detector/anagramDetector.js
@@ -11,36 +11,67 @@ Suggestions
 Start out by getting this working with discrete functions.
 // If you feel comfortable with that, try to put all your functions, collections, etc, into an object.*/
 
-let anagrams = [];
+// let anagrams = [];
 
-const customSort = function(word) {
-  word = word.split("").sort().join("");
+// const customSort = function(word) {
+//   word = word.split("").sort().join("");
 
-  return word
-}
+//   return word
+// }
 
-const checkForAnagram = function(word, candidateWords) {
-  // word = word.split("");
-  // console.log(word);
-  // word = word.sort();
-  // console.log(word);
-  // word = word.join("");
-  // console.log(word);
+// const checkForAnagram = function(word, candidateWords) {
+//   // word = word.split("");
+//   // console.log(word);
+//   // word = word.sort();
+//   // console.log(word);
+//   // word = word.join("");
+//   // console.log(word);
 
-  let sortedWord = customSort(word)
-  console.log(sortedWord);
+//   let sortedWord = customSort(word)
+//   console.log(sortedWord);
 
-  for ( let i = 0; i < candidateWords.length; i++ ) {
-    console.log("word before sort: ", candidateWords[i]);
-    let candidate = customSort(candidateWords[ i ]);
-    console.log("word after sort: ", candidate);
+//   for ( let i = 0; i < candidateWords.length; i++ ) {
+//     console.log("word before sort: ", candidateWords[i]);
+//     let candidate = customSort(candidateWords[ i ]);
+//     console.log("word after sort: ", candidate);
 
-    if (sortedWord === candidate) {
-      anagrams.push(candidateWords[i])
-    }
-  } //for
+//     if (sortedWord === candidate) {
+//       anagrams.push(candidateWords[i])
+//     }
+//   } //for
 
-  console.log(`The anagram of ${word} is ${anagrams}`);
-}; //chekcforanagram
+//   console.log(`The anagram of ${word} is ${anagrams}`);
+// }; //chekcforanagram
 
-console.log(checkForAnagram("listen", ["enlists", "google", "inlets", "banana"]));
+// console.log(checkForAnagram("listen", ["enlists", "google", "inlets", "banana"]));
+
+
+// Version 2
+
+const anagramChecker = {
+  anagrams: [],
+
+  customSort: function(word) {
+    return word.split("").sort().join("");
+  },
+
+  checkForAnagram: function(word, candidateWords) {
+
+     let sortedWord = this.customSort(word)
+     console.log(sortedWord);
+
+     for ( let i = 0; i < candidateWords.length; i++ ) {
+       console.log("word before sort: ", candidateWords[i]);
+       let candidate = this.customSort(candidateWords[ i ]);
+       console.log("word after sort: ", candidate);
+
+       if (sortedWord === candidate) {
+         this.anagrams.push(candidateWords[i])
+       }
+     }
+
+     console.log(`The anagram of ${word} is ${this.anagrams}`);
+   } //chekcforanagram
+};
+
+console.log(anagramChecker.checkForAnagram("listen", ["enlists", "google", "inlets", "banana"]));
