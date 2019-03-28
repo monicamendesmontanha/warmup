2902cc9860564a40b67f2514f3a1baab59b7ba34
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-29 10:03:54 +1100

    solution of space age (for...in)

--- 09_spaceAge/spaceAge.js
+++ 09_spaceAge/spaceAge.js
@@ -6,7 +6,7 @@ console.log(
 Space Age Warmup
 Given an age in seconds, calculate how old someone would be on the following planets:
 
-Earth: orbital period 365.25 Earth days
+Earth: orbital period 365.25 Earth days // 1 day
 Mercury: orbital period 0.2408467 Earth years
 Venus: orbital period 0.61519726 Earth years
 Mars: orbital period 1.8808158 Earth years
@@ -14,7 +14,8 @@ Jupiter: orbital period 11.862615 Earth years
 Saturn: orbital period 29.447498 Earth years
 Uranus: orbital period 84.016846 Earth years
 Neptune: orbital period 164.79132 Earth years
-So if you were told someone were 1,000,000,000 seconds old, you should be able to say:
+
+>> So if you were told someone were 1,000,000,000 seconds old, you should be able to say:
 
 You are 31.68808781402895 Earth-years old
 You are 131.56953287725742 Mercury-years old
@@ -24,10 +25,32 @@ You are 2.6712565327315225 Jupiter-years old
 You are 1.0760876124018737 Saturn-years old
 You are 0.37716350140100413 Uranus-years old
 You are 0.1922922142624317 Neptune-years old
-There are 31557600 seconds in an Earth year.
+
+>> There are 31557600 seconds in an Earth year.
 
 Bonus:
 Have the option of either returning the ages on all planets, or any of the above planets individually.
 */
 
-
+// version 01
+const spaceAge = {
+  data: {
+    Earth: 1,
+    Mercury: 0.2408467,
+    Venys: 0.61519726,
+    Mars: 1.8808158,
+    Jupiter: 11.862615,
+    Saturn: 29.447498,
+    Uranus: 84.016846,
+    Neptune: 164.79132
+  },
+  calculate: function(input) {
+    for (let key in this.data) {
+      let earthYears = input / 31557600;
+      let value = this.data[key];
+      let age = (earthYears / value).toFixed(2)
+      console.log(`Your age on ${key} is ${age}`);
+    }
+  }
+}
+console.log(spaceAge.calculate(1000000000));
