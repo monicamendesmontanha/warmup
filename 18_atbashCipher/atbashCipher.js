5d02f0db9d6daf94c14efd227a59f677fa07ed61
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-01 10:45:06 +1000

    solution version 2

--- 18_atbashCipher/atbashCipher.js
+++ 18_atbashCipher/atbashCipher.js
@@ -3,34 +3,58 @@ console.log(
   "color: blue; background-color: yellow;"
 );
 
-const cipher = function(text) {
+//version 1
 
-  //ABCDEF to QWERTY map
-  var map = {
-    a: 'z', b: 'y', c: 'x',
-    d: 'w', e: 'v', f: 'u',
-    g: 't', h: 's', i: 'r',
-    j: 'q', k: 'p', l: 'o',
-    m: 'n', n: 'm', o: 'l',
-    p: 'k', q: 'j', r: 'i',
-    s: 'h', t: 'g', u: 'f',
-    v: 'e', w: 'd', x: 'c',
-    y: 'b', z: 'a'
-  };
+// const cipher = function(text) {
 
-  // Convert to an array of characters, filter, iterate and finally join
-  return text.split('').filter(function(v) {
-    // Does the character exist in the map?
-    // Filter out characters that are not in our list
-    return map.hasOwnProperty(v.toLowerCase());
-  }).map(function(v) {
-    // Replace old character by new one
-    // And make it uppercase to make it look fancier
-    return map[v.toLowerCase()].toUpperCase();
-  }).join('');
+//   //ABCDEF to map
+//   var map = {
+//     a: 'z', b: 'y', c: 'x',
+//     d: 'w', e: 'v', f: 'u',
+//     g: 't', h: 's', i: 'r',
+//     j: 'q', k: 'p', l: 'o',
+//     m: 'n', n: 'm', o: 'l',
+//     p: 'k', q: 'j', r: 'i',
+//     s: 'h', t: 'g', u: 'f',
+//     v: 'e', w: 'd', x: 'c',
+//     y: 'b', z: 'a'
+//   };
 
+//   // Convert to an array of characters, filter, iterate and finally join
+//   return text.split('').filter(function(v) {
+//     // Does the character exist in the map?
+//     // Filter out characters that are not in our list
+//     return map.hasOwnProperty(v.toLowerCase());
+//   }).map(function(v) {
+//     // Replace old character by new one
+//     // And make it uppercase to make it look fancier
+//     return map[v.toLowerCase()].toUpperCase();
+//   }).join('');
+
+// };
+
+// var text = 'test';
+// var encoded = cipher(text); // SVOOLDLIOW
+// console.log(encoded)
+
+//version 2
+
+const atbash = {
+  alphabet: "abcdefghijklmnopqrstuvwxyz".split(''),
+
+  encode: function(input) {
+    output = "";
+
+    input = input.split("");
+
+    for ( let i = 0; i < input.length; i++ ) {
+      const index = this.alphabet.indexOf( input[i] )
+      output += this.alphabet.reverse()[index]
+    }
+
+    return output
+  }
 };
 
-var text = 'test';
-var encoded = cipher(text); // SVOOLDLIOW
-console.log(encoded)
+console.log(atbash.encode('test'));
+console.log(atbash.encode('gvhg'));
