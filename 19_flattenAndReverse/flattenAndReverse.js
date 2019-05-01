bdf70979ca6e190062a8610214bab3b8b58e824b
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-02 09:17:09 +1000

    solution hot to flatten and reverse an array

---
+++ 19_flattenAndReverse/flattenAndReverse.js
@@ -0,0 +1,13 @@
+console.log(
+  "%c--Flatten and Reverse, Warmup Exercise--",
+  "color: blue; background-color: yellow;"
+);
+
+
+myArray = [1, 2, 3, 4]
+
+console.log(myArray.reverse())
+
+myFlat = ["Hello", ["World", 42] ]
+
+console.log(myFlat.flat())
