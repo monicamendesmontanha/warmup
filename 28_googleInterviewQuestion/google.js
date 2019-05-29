58c43403f7c54e4574126464d9fe33db8919e18b
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-29 10:14:22 +1000

    3 aversion with recursion

--- 28_googleInterviewQuestion/google.js
+++ 28_googleInterviewQuestion/google.js
@@ -6,25 +6,25 @@ console.log(
 /////////////
 // 1ª version
 /////////////
-const findSum = (arr, val) => {
-  let searchValues = new Set();
-  searchValues.add(val - arr[0])
+// const findSum = (arr, val) => {
+//   let searchValues = new Set();
+//   searchValues.add(val - arr[0])
 
-  for (let i = 1; i < arr.length; i++) {
-    let searchVal = val - arr[i]
+//   for (let i = 1; i < arr.length; i++) {
+//     let searchVal = val - arr[i]
 
-    if (searchValues.has(arr[i])) {
-      return true;
-    } else {
-      searchValues.add(searchVal)
-    }
-  };
+//     if (searchValues.has(arr[i])) {
+//       return true;
+//     } else {
+//       searchValues.add(searchVal)
+//     }
+//   };
 
-  return false
-};
+//   return false
+// };
 
-console.log(findSum([3,5,1,4], 9));
-console.log(findSum([3,5,1,4], 2));
+// console.log(findSum([3,5,1,4], 9)); //true
+// console.log(findSum([3,5,1,4], 2)); //false
 
 /////////////
 // 2ª version
@@ -41,5 +41,40 @@ console.log(findSum([3,5,1,4], 2));
 //   return false;
 // };
 
-// console.log( google([3, 5, 1, 4], 9) );
-// console.log( google([3, 5, 1, 4], 2) );
+// console.log( google([3, 5, 1, 4], 9) ); //true
+// console.log( google([3, 5, 1, 4], 2) ); //false
+
+
+/////////////
+// 3ª version
+/////////////
+
+const isSum = ( arr, sum ) => {
+
+  function individualCheck( base=0, current=1 ) {
+    // console.log(`individualCheck with base: ${ base } and current: ${ current }`);
+    while ( base < arr.length ) {
+      if ( base === current && base !== arr.length - 1 ) {
+        // console.log('Checking with the number itself');
+      } else
+      if ( sum === arr[base] + arr[current] ) {
+        return true;
+      } else if ( base === arr.length - 1 && current === arr.length - 1 ) {
+        return false;
+      }
+
+      if ( current >= arr.length - 1 ) {
+        base += 1;
+        current = 0;
+      } else {
+        current += 1;
+      }
+      individualCheck( base, current );
+    }
+  }
+
+  return individualCheck();
+}
+
+console.log( isSum([3, 5, 1, 4], 9) ); // true
+console.log( isSum([3, 5, 1, 4], 2) ); // false
