026f3526204f6baefd5f66a878a9b209d59cda05
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-29 10:04:02 +1000

    Solucao version 2

--- 28_googleInterviewQuestion/google.js
+++ 28_googleInterviewQuestion/google.js
@@ -3,22 +3,43 @@ console.log(
   "color: blue; background-color: yellow;"
 );
 
-const findSum = (arr, val) => {
-  let searchValues = new Set();
-  searchValues.add(val - arr[0])
+/////////////
+// 1ª version
+/////////////
+// const findSum = (arr, val) => {
+//   let searchValues = new Set();
+//   searchValues.add(val - arr[0])
 
-  for (let i = 1; i < arr.length; i++) {
-    let searchVal = val - arr[i]
+//   for (let i = 1; i < arr.length; i++) {
+//     let searchVal = val - arr[i]
 
-    if (searchValues.has(arr[i])) {
-      return true;
-    } else {
-      searchValues.add(searchVal)
-    }
-  };
+//     if (searchValues.has(arr[i])) {
+//       return true;
+//     } else {
+//       searchValues.add(searchVal)
+//     }
+//   };
 
-  return false
+//   return false
+// };
+
+// console.log(findSum([3,5,1,4], 9));
+// console.log(findSum([3,5,1,4], 2));
+
+/////////////
+// 2ª version
+/////////////
+const google = (array, sum) => {
+
+  for (let i = array.length-1; i > 0; i--) {
+      let val = array.pop();
+      if ( array.includes( (sum - val) ) ) {
+          return true;
+      }
+  }
+
+  return false;
 };
 
-console.log(findSum([3,5,1,4], 9));
-console.log(findSum([3,5,1,4], 2));
+console.log( google([3, 5, 1, 4], 9) );
+console.log( google([3, 5, 1, 4], 2) );
