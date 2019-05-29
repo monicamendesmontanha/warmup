93c24be40b3ccab51dd0c56eee2d674a74493f19
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-29 10:05:39 +1000

    keep the first solution

--- 28_googleInterviewQuestion/google.js
+++ 28_googleInterviewQuestion/google.js
@@ -6,40 +6,40 @@ console.log(
 /////////////
 // 1ª version
 /////////////
-// const findSum = (arr, val) => {
-//   let searchValues = new Set();
-//   searchValues.add(val - arr[0])
+const findSum = (arr, val) => {
+  let searchValues = new Set();
+  searchValues.add(val - arr[0])
 
-//   for (let i = 1; i < arr.length; i++) {
-//     let searchVal = val - arr[i]
+  for (let i = 1; i < arr.length; i++) {
+    let searchVal = val - arr[i]
 
-//     if (searchValues.has(arr[i])) {
-//       return true;
-//     } else {
-//       searchValues.add(searchVal)
-//     }
-//   };
+    if (searchValues.has(arr[i])) {
+      return true;
+    } else {
+      searchValues.add(searchVal)
+    }
+  };
 
-//   return false
-// };
+  return false
+};
 
-// console.log(findSum([3,5,1,4], 9));
-// console.log(findSum([3,5,1,4], 2));
+console.log(findSum([3,5,1,4], 9));
+console.log(findSum([3,5,1,4], 2));
 
 /////////////
 // 2ª version
 /////////////
-const google = (array, sum) => {
+// const google = (array, sum) => {
 
-  for (let i = array.length-1; i > 0; i--) {
-      let val = array.pop();
-      if ( array.includes( (sum - val) ) ) {
-          return true;
-      }
-  }
+//   for (let i = array.length-1; i > 0; i--) {
+//       let val = array.pop();
+//       if ( array.includes( (sum - val) ) ) {
+//           return true;
+//       }
+//   }
 
-  return false;
-};
+//   return false;
+// };
 
-console.log( google([3, 5, 1, 4], 9) );
-console.log( google([3, 5, 1, 4], 2) );
+// console.log( google([3, 5, 1, 4], 9) );
+// console.log( google([3, 5, 1, 4], 2) );
